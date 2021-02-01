edcc5399bfa001ce6fabb4500e39b5e8c182ee9a
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-02-01 14:29:25 -0700

    added last modified to server

--- checkpoint-6.server/server/models/Bug.js
+++ checkpoint-6.server/server/models/Bug.js
@@ -6,8 +6,8 @@ const Bug = new Schema({
   description: { type: String, required: true },
   title: { type: String, required: true },
   closedDate: { type: Date },
-  // creatorEmail: { type: String, required: true },
-  creatorId: { type: String, ref: 'Account', required: true }
+  creatorId: { type: String, ref: 'Account', required: true },
+  lastModified: { type: String, required: true }
 },
 { timestamps: true, toJSON: { virtuals: true } }
 )
