b22fb68c666ec1726e9514e4353f60649f3f688b
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-03-18 15:44:46 -0600

    some changes

--- checkpoint-6.client/src/pages/LandingPage.vue
+++ checkpoint-6.client/src/pages/LandingPage.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="container">
+    <div class="row text-center m-5">
+      <div class="col">
+        <h1><i class="fas fa-bug text-success"></i> Bug Log</h1>
+      </div>
+    </div>
     <div class="row text-center">
       <div class="col">
-        <h1><i class="fas fa-bug text-success"></i>Bug Log</h1>
+        <p>Please login to Begin</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { computed, reactive } from 'vue'
+import { AppState } from '../AppState'
 export default {
-  name: 'Landing'
+  name: 'Landing',
+  setup() {
+    const state = reactive({
+      user: computed(() => AppState.user)
+    })
+    return {
+      state
+    }
+  }
 }
 </script>
 
