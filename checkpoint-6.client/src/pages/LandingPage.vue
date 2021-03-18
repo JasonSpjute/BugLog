8186053fec7751d71a68afea1d0dfc9f24651884
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-03-18 16:05:40 -0600

    background image and begin button

--- checkpoint-6.client/src/pages/LandingPage.vue
+++ checkpoint-6.client/src/pages/LandingPage.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <div class="row text-center m-5">
       <div class="col">
         <h1><i class="fas fa-bug text-success"></i> Bug Log</h1>
       </div>
     </div>
     <div class="row text-center">
-      <div class="col">
+      <div class="col" v-if="state.user.isAuthenticated">
+        <button>Go to Bug Page</button>
+      </div>
+      <div class="col" v-else>
         <p>Please login to Begin</p>
       </div>
     </div>
@@ -30,5 +33,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .container-fluid{
+    background-image: url(https://fourdeeroak.files.wordpress.com/2012/04/green-bug-on-log-pp.jpg);
+    background-size: cover;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+  }
 </style>
